e7677cf64621796df43e688f3a1ae795b42a15d6
Kuri01 <kamil.kuriata01@gmail.com>
2021-11-05 22:23:10 +0100

    12.3 Hero

---
+++ src/components/List/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from '../Hero/Hero';
+// import styles from './styles/styles.scss';
+
+class List extends React.Component {
+  render() {
+    return (
+      <section>
+        <Hero />
+      </section>
+    );
+  }
+}
+
+export default List;
