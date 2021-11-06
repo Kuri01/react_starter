b825fd56886ba95eacf29505043eb454a9f45b76
Kuri01 <kamil.kuriata01@gmail.com>
2021-11-06 02:04:56 +0100

    12.4 Props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
-// import styles from './styles/styles.scss';
+import styles from './List.scss';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    children: PropTypes.node,
+  };
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  };
   render() {
     return (
       <section>
-        <Hero />
+        <Hero titleText={this.props.title} imageSource={this.props.image} />
+        <div className={styles.description}>{this.props.children}</div>
+
+        <div className={styles.columns}>
+          <Column title={'First'} />
+          <Column title={'Second'} />
+          <Column title={'Third'} />
+        </div>
       </section>
     );
   }
